96b68d3c4073b38026e9cdc96834e72fe03b35bb
Shirlesha <1251671603@qq.com>
2019-04-17 09:58:50 +0800

    不知道又做了什么

--- demo01/booklist/models.py
+++ demo01/booklist/models.py
@@ -5,6 +5,9 @@ class BookInfo(models.Model):
     bname = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.bname
+
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
@@ -12,6 +15,9 @@ class HeroInfo(models.Model):
     hcontent = models.CharField(max_length=100)
     # 外键 第一个参数为表名 第二个参数代表删除类型
     hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hname
 
 
 
